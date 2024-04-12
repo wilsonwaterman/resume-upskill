4cf619e6bd10e146c9f24bfb8bd590639661f396
wilsonwaterman <wilsonwaterman@gmail.com>
2024-04-12 09:45:32 -0600

    move test env arn to variables file

--- iac/test/vars.tf
+++ iac/test/vars.tf
@@ -5,3 +5,7 @@ variable "AWS_REGION" {
 variable "BUCKET_NAME" {
     default = "wwaterman-test-bucket-778"
 }
+
+variable "ACM_TEST_ARN" {
+    default = "arn:aws:acm:us-east-1:649148530717:certificate/d5f8829f-79f4-48de-9fb5-2b2207a13e61"
+}
